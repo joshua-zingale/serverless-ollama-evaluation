712a1d531b50bbd98d766a96fe4a6b72dafe850d
Joshua Zingale <jtzingale@gmail.com>
2025-03-06 00:34:53 -0800

    Refactored Client

--- client/client.py
+++ client/client.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from helpers import *
 import asyncio
 import pprint
+import json
 
 parser = ArgumentParser()
 
@@ -10,49 +11,51 @@ parser.add_argument("--seed", type=int, default=None)
 
 args = parser.parse_args()
 
-async def ask(question: str, start_time, countdown: Countdown) -> dict:
+async def ask(question: str, start_time, clock: Clock) -> dict:
     '''
-    Streams responses from a language model for a question until the countdown is finished.
+    Streams responses from a language model for a question
 
     Returns statistics within a dictionary about the generation:
      - request_time: time in seconds since the countdown's start that the first request was sent to the LLM
      - num_chars: number of characters generated from the language model before finishing, i.e. before either the generation or countdown ended
      - ttft: time in seconds since the request that the first token was received
-     - finished: boolan, True if the response finished generating before the countdown finished; otherwise False
      - finish_time: time in seconds since the countdown's start that the last token was received form the LLM
     '''
-    stats = {'request_time': start_time, 'finished': True, 'num_chars': 0}
+    stats = {'request_time': start_time, 'num_chars': 0}
     first = True
     async for chunk in await answer(question):
         if first and len(chunk['response']) > 0:
-             first = False
-             stats['ttft'] = countdown.time_elasped() - start_time
+            first = False
+            stats['ttft'] = clock.time() - start_time
 
-        if countdown.finished():
-                stats['finished'] = False
-                break
-        
         stats['num_chars'] += len(chunk['response'])
 
-    stats['finish_time'] = countdown.time_elasped()
+    stats['finish_time'] = clock.time()
 
     return stats
 
 async def main():
     
     questions = QuestionSet()
-    countdown = Countdown(10)
+    clock = Clock()
     tasks = []
-    for question in countdown.bind(questions):
-        task = asyncio.create_task(ask(question, countdown.time_elasped(), countdown))
+    async for question in AsyncPacer(questions, clock, lambda x: 1 if x < 15 else None):
+        task = asyncio.create_task(ask(question, clock.time(), clock))
         tasks.append(task)
-        await asyncio.sleep(1)
 
-    results = await asyncio.gather(*tasks)
+    pending_tasks = asyncio.all_tasks()
+    for task in pending_tasks:
+        if task is not asyncio.current_task():
+            task.cancel()
 
-    for result in sorted(results, key=lambda x: x['request_time']):
-         pprint.pprint(result)
-         print()
+    results = sorted(
+        filter(lambda x: not isinstance(x, asyncio.CancelledError),
+               await asyncio.gather(*tasks, return_exceptions=True)),
+                    key=lambda x: x['request_time'])
+    
+    filename = str(time.time()).replace(".","-")
+    with open(f"data/results/{filename}.json", "w") as f:
+        json.dump(results, f)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- client/helpers.py
+++ client/helpers.py
@@ -2,7 +2,7 @@ from config import OLLAMA_URL, MODEL_NAME
 from ollama import AsyncClient
 import random
 import time
-
+import asyncio
 
 client = AsyncClient(
   host=OLLAMA_URL,
@@ -10,26 +10,76 @@ client = AsyncClient(
 
 def answer(prompt: str):
     '''
-    Send a prompt to Ollama to get a streaming response from the language model.
+    Send a prompt to Ollama to get an asynchronous streaming response from the language model.
     '''
     return client.generate(model=MODEL_NAME, prompt = prompt, stream = True)
 
 class QuestionSet():
     '''An iterable set of natural langauge questions that yields questions'''
-    def __init__(self, seed = None):
+    def __init__(self, n: int = 1000, seed = None):
         self.seed = seed
         self.questions = []
         with open("data/questions") as f:
-            for line in f:
+            for i, line in enumerate(f):
+                if i == n:
+                    break
                 self.questions.append(line[:-1])
 
+    def get(self):
+        return self.questions[random.randint(0, len(self.questions) - 1)]
+    
     def __iter__(self):
         random.seed(self.seed)
 
         while True:
             yield self.questions[random.randint(0, len(self.questions) - 1)]
 
-    
+
+class AsyncPacer():
+    """
+    Wraps around any generator to make a rate-limited asyncronous iterator.
+
+    The iterator will continue to provide a next element until
+    either the generator runs out of elements or until the pacing
+    function returns None.
+
+    The pacing function determines how much time is artificially placed
+    between each iteration with a non-blocking sleep.
+    """
+    def __init__(self, generator, clock, pacing_function):
+        self.generator = generator
+        self.pacing_function = pacing_function
+        self.clock = clock
+
+    async def __aiter__(self):
+       
+       before_gen = self.clock.time()
+       for e in self.generator:
+            yield e
+            wait_time = self.pacing_function(self.clock.time())
+            if not wait_time:
+                break
+            
+            generation_time = self.clock.time() - before_gen
+            await asyncio.sleep(max(wait_time - generation_time, 0))
+            before_gen = self.clock.time()
+            
+        
+
+
+
+class Clock():
+        def __init__(self):
+            self.start_time = time.time()
+            self.running = True
+        def time(self):
+            return time.time() - self.start_time
+        def stop(self):
+            self.running = False
+        def running(self):
+            return self.running
+
+
 class Countdown():
     def __init__(self, length: float):
         self.start_time = time.time()
