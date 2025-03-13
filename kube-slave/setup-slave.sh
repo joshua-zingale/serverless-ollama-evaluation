3b98b2d1cabfa0788b6437b43606312dcb0fc02f
Joshua Zingale <jtzingale@gmail.com>
2025-03-12 17:24:59 -0700

    slave setup

--- kube-slave/setup-slave.sh
+++ kube-slave/setup-slave.sh
@@ -48,3 +48,5 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo crictl pull docker.io/jzing002/cs208-server
