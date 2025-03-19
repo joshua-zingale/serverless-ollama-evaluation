934b52bfea3697914f0441b2d46f48876e37a960
Joshua Zingale <jtzingale@gmail.com>
2025-03-18 17:54:56 -0700

    Memory data & 12 pods

--- client/pull_prometheus_data.py
+++ client/pull_prometheus_data.py
@@ -7,9 +7,11 @@ def main():
 
     parser.add_argument('url')
     parser.add_argument('start_time', type=float)
-    parser.add_argument('end_time', type=float)
+    parser.add_argument('length', type=float)
 
     args = parser.parse_args()
+
+    args.end_time = args.start_time + args.length
 
     params = {
         'query': 'count(kube_pod_container_status_running{pod=~"ollama.*"})/2 or vector(0)',
@@ -33,11 +35,20 @@ def main():
         data2 = json.loads(resp.text)
 
 
+    params = {
+        'query': 'sum(container_memory_usage_bytes{pod=~"ollama.*"}) or vector(0)',
+        'step': '10s',
+        'start': args.start_time,
+        'end': args.end_time
+
+    }
+    with requests.get(f"{args.url}/api/v1/query_range", params=params) as resp:
+        data3 = json.loads(resp.text)
+
     new_data = {'start_time': args.start_time, 'data': []}
 
-    for (time, num_pods), (_, cpu_seconds_per_second), in zip(data['data']['result'][0]['values'], data2['data']['result'][0]['values']):
-        new_data['data'].append([time - args.start_time, int(num_pods), float(cpu_seconds_per_second)])
-
+    for (time, num_pods), (_, cpu_seconds_per_second), (_, memory_usage) in zip(data['data']['result'][0]['values'], data2['data']['result'][0]['values'], data3['data']['result'][0]['values']):
+        new_data['data'].append([time - args.start_time, int(num_pods), float(cpu_seconds_per_second), int(memory_usage)])
 
     print(json.dumps(new_data))
 
